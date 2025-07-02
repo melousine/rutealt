a45cd81e46182e1f0e7abb6199fa2eefeeeae29f
melousine <rafifasarahs@gmail.com>
2025-07-02 23:38:17 +0700

    fix host2

--- app.py
+++ app.py
@@ -227,5 +227,5 @@ def index():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8080))  # ambil port dari env
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
